783d4ccbbec96cbe08f4741e670b749e182a099f
Komal_Goel <126601911+komal-Goel-04@users.noreply.github.com>
2023-03-10 11:55:08 -0500

    Update week1sumdigit.c

--- week1sumdigit.c
+++ week1sumdigit.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int a,remainder=0,sum=0;
+   int a,remainder=0,sum=0;
     printf("Enter a number :");
     scanf("%d",&a);
     do
     {
-        sum=sum+remainder;
         remainder=a%10;
         a=a/10;
+        sum=sum+remainder;
     }
     while(a>0);
     printf("Sum of digits = %d",sum);
