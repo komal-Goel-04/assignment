102220660157376402408cb29aac9b3136864cae
Komal_Goel <126601911+komal-Goel-04@users.noreply.github.com>
2023-03-10 12:09:54 -0500

    Update week1switch.c

--- week1switch.c
+++ week1switch.c
@@ -1,31 +1,43 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,ans=0;
-    int op=1;
+    int op;
+    float ans,a,b;
     printf("Enter two numbers :");
-    scanf("%d %d",&a,&b);
+    scanf("%f %f",&a,&b);
+    printf("1 -> Addition   2 -> Subtraction    3 -> Multiplication     4 -> Division\n");
+    printf("enter your choice :\n");
+    scanf("%d",&op);
     switch(op)
     {
         case 1:
         {
             ans=a+b;
-            printf("%d + %d = %d\n",a,b,ans);
+            printf("%f + %f = %f\n",a,b,ans);
+            break;
         }
         case 2:
         {
             ans=a-b;
-            printf("%d - %d = %d\n",a,b,ans);
+            printf("%f - %f = %f\n",a,b,ans);
+            break;
         }
         case 3:
         {
             ans=a*b;
-            printf("%d * %d = %d\n",a,b,ans);
+            printf("%f * %f = %f\n",a,b,ans);
+            break;
         }
         case 4:
         {
             ans=a/b;
-            printf("%d / %d = %d\n",a,b,ans);
+            printf("%f / %f = %f\n",a,b,ans);
+            break;
+        }
+        default :
+        {
+            ("you have entered wrong choice");
+            break;
         }
     }
 }
