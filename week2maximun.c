0991314292adb988ec0b9fe7c42f5df21e2cb183
Komal_Goel <126601911+komal-Goel-04@users.noreply.github.com>
2023-03-19 01:00:44 -0500

    Update week2maximun.c

--- week2maximun.c
+++ week2maximun.c
@@ -11,33 +11,22 @@ int main()
     {
         scanf("%d",&arr[i]);
     }
-
-    int ans[10];
-    for(int i=0;i<10;i++)
+    int maxcount=0,maxelement;
+    for(int i=0;i<size-1;i++)
     {
-        int count=0;
-        for(int j=0;j<size;j++)
+        int count=1;
+        for(int j=i+1;j<size;j++)
         {
-            if(arr[j] == i)
-            count++;
-        }
-        ans[i]=count;
-    }
-    
-    int max=0;
-    for(int i=0;i<9;i++)
-    {
-        if(ans[i]>max)
-        max = ans[i];
-        
-    }
-
-    for(int i=0;i<9;i++)
-    {
-        if(max == ans[i])
-        {
-        printf("maximun occuring integer is : %d",i);
-        break;
+            if(arr[i]==arr[j])
+            {
+                count++;
+                if(count>maxcount)
+                {
+                    maxelement=arr[j];
+                    maxcount=count;
+                }
+            }
         }
     }
+    printf("maximum occuring integer is : %d",maxelement);
 }
