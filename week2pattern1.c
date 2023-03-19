6dd0683a45c102fb85c7563a3bc2e8198b2c1a91
Komal_Goel <126601911+komal-Goel-04@users.noreply.github.com>
2023-03-18 22:35:15 -0500

    Update week2pattern1.c

--- week2pattern1.c
+++ week2pattern1.c
@@ -5,7 +5,7 @@ int main()
     int totalrows;
     printf("enter total number of rows to be printed : ");
     scanf("%d",&totalrows);
-    for(int row = 1 ; row < totalrows ; row++)
+    for(int row = 1 ; row <= totalrows ; row++)
     {
         for(int col = 1 ; col <= row ; col++)
         {
