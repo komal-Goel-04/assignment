41b18ece954229499cba43c9eca6b3fa6aa14fa1
Komal_Goel <126601911+komal-Goel-04@users.noreply.github.com>
2023-04-02 11:22:24 -0500

    Update week3removechar.c

--- week3removechar.c
+++ week3removechar.c
@@ -6,17 +6,13 @@ int main()
     char c;
     printf("enter a string : ");
     gets(str);
-    printf("enter the character you want to find : ");
+    printf("enter the character you want to remove : ");
     scanf("%c", &c);
     int length = strlen(str);
     int ans = 0;
     for (int i = 0; i < length; i++)
     {
-        if (str[i] == c)
-            ans = 1;
+        if (str[i] != c)
+            printf("%c", str[i]);
     }
-    if (ans == 1)
-        printf("%c is present", c);
-    else
-        printf("%c is not present", c);
 }
